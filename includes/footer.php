141f802da911f0b9ac7af0c06257b75b99d39531
Naimur <naimurshuvo.bd.16@gmail.com>
2025-02-01 14:27:42 +0600

    Mobile View Responsible Toggle Added

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,5 @@
 </div>
+  <!-- Add Bootstrap JS -->
+  <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -3,26 +3,45 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Event Management System</title>
+  <title>Event System</title>
   <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
+      <!-- Brand/Logo -->
       <a class="navbar-brand" href="dashboard.php">Event System</a>
-      <div class="collapse navbar-collapse">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="dashboard.php">Dashboard</a>
-          </li>
+      
+      <!-- Hamburger Toggle Button for Mobile -->
+      <button 
+        class="navbar-toggler" 
+        type="button" 
+        data-bs-toggle="collapse" 
+        data-bs-target="#navbarNav" 
+        aria-controls="navbarNav" 
+        aria-expanded="false" 
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+    
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto"> 
           <?php if (isset($_SESSION['user_id'])): ?>
             <li class="nav-item">
+              <a class="nav-link" href="dashboard.php">Dashboard</a>
+            </li>
+            <li class="nav-item">
               <a class="nav-link" href="logout.php">Logout</a>
+            </li>
+          <?php else: ?>
+            <li class="nav-item">
+              <a class="nav-link" href="login.php">Login</a>
             </li>
           <?php endif; ?>
         </ul>
       </div>
     </div>
   </nav>
-  <div class="container">
-
+  <div class="container mt-4">
